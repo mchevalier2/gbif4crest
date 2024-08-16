88137a0a30bcdd4176771d7541f068dc6006a6e8
Manu Chevalier <it@manuelchevalier.com>
2024-08-16 16:39:42 +0200

    Improving resilience of the data extraction from API

--- build_database_from_gbif_data.py
+++ build_database_from_gbif_data.py
@@ -4,79 +4,134 @@ import time
 import pandas as pd
 from pygbif import occurrences as occ
 
+DATA_FOLDER = "./data/"
+TEMP_FOLDER = "./tmp/"
+DATABASE_FOLDER = "./database_files/"
 
 
-DATA_FOLDER = './data/'
-TEMP_FOLDER = './tmp/'
-DATABASE_FOLDER = './database_files/'
-
-
-
-if DATA_FOLDER not in os.listdir():
+try:
     os.mkdir(DATA_FOLDER)
+except FileExistsError:
+    pass
 
-if TEMP_FOLDER not in os.listdir():
+
+try:
     os.mkdir(TEMP_FOLDER)
-
+except FileExistsError:
+    pass
 
 
 ## CREATING TAXALIST TABLE ====================================================>
+print("\n\n\nCREATING TAXALIST TABLE")
 
-LIST_OF_TAXALIST_FILES = [x for x in os.listdir(DATA_FOLDER) if x.startswith('taxalist_')]
-#taxalist = pd.concat((pd.read_csv(DATA_FOLDER+f) for f in LIST_OF_TAXALIST_FILES), ignore_index=True)
+
+def create_ID_from_taxonomy(x):
+    a, b, d = x
+    ## Plants
+    if a in ["Tracheophyta", "Anthocerotophyta", "Bryophyta", "Marchantiophyta"]:
+        return 1000001 + d
+    ## Forams
+    if a in ["Foraminifera"]:
+        return 4000001 + d
+    ## Diatoms
+    if b in ["Bacillariophyceae"]:
+        return 5000001 + d
+    ## Mammals, including rodents
+    if b in ["Mammalia"]:
+        return 6000001 + d
+    return -1
+
+
+LIST_OF_TAXALIST_FILES = [
+    x for x in os.listdir(DATA_FOLDER) if x.startswith("taxalist_")
+]
 taxalist = (
-            pd.concat(
-                    (pd.read_csv(DATA_FOLDER+f) for f in LIST_OF_TAXALIST_FILES),
-                    ignore_index=True
-                )
-               .drop_duplicates()
-               .sort_values(['kingdom','phylum','class_name', \
-                             'order_name','family','genus','species'])
+    pd.concat(
+        (pd.read_csv(DATA_FOLDER + f) for f in LIST_OF_TAXALIST_FILES),
+        ignore_index=True,
+    )
+    .drop_duplicates()
+    .sort_values(
+        ["kingdom", "phylum", "class_name", "order_name", "family", "genus", "species"]
+    )
 )
-taxalist['newID'] = taxalist.groupby(['kingdom']).agg({'taxonID':'cumcount'})
-print("\n\nWARNING: I still need to 1) rename newID to something better and 2) to encode the type of taxon in the newID.\n\n")
-taxalist = taxalist.sort_values('newID')
-taxalist.to_csv(DATABASE_FOLDER+'taxalist.csv', mode="w", header=True, index=False)
+taxalist["gbifID"] = taxalist["taxonID"]
+taxalist["newID"] = taxalist.groupby(["kingdom"]).agg({"taxonID": "cumcount"})
+taxalist["taxonID"] = taxalist[["phylum", "class_name", "newID"]].apply(
+    create_ID_from_taxonomy, axis=1
+)
+taxalist = taxalist.sort_values("taxonID")
+taxalist["gbifID"] = taxalist["gbifID"].astype("Int64")
+taxalist = taxalist.drop("newID", axis=1).reset_index(drop=True)
 
+assert (
+    taxalist[taxalist["taxonID"] == -1].shape[0] == 0
+), ["\nSome taxa did not get a proper taxonID.", taxalist.query("taxonID==-1")]
+taxalist.to_csv(DATABASE_FOLDER + "taxalist.csv", mode="w", header=True, index=False)
+print(taxalist)
 # <============================================================================
 
 
-
-
-
 ## CREATING DISTRIB TABLE =====================================================>
-
+print("\n\n\nCREATING DISTRIB TABLE")
 # Loading the list of files to download from GBIF
-with open(DATA_FOLDER+'download_list.txt') as f:
+with open(DATA_FOLDER + "download_list.txt") as f:
     LIST_OF_DATA_FILES = [x[:-1] for x in f.readlines()]
 
 # For each file in that list, do
 for datafile in LIST_OF_DATA_FILES:
-    keeptrying=True
+    print("Analysing: " + datafile)
+    keeptrying = True if datafile + ".zip" not in os.listdir(DATA_FOLDER) else False
     while keeptrying:
         try:
             occ.download_get(datafile, path=DATA_FOLDER)
-            keeptrying=False
+            keeptrying = False
         except:
             time.sleep(600)
-    gbif_data = pd.read_csv(DATA_FOLDER + datafile+'.zip', sep='\t')
-    gbif_data = gbif_data[['speciesKey', 'decimalLongitude', 'decimalLatitude', 'year', 'basisOfRecord']].drop_duplicates()
-    gbif_data = gbif_data.merge(taxalist, left_on='speciesKey', right_on='taxonID', how='left')
-    gbif_data = gbif_data[['newID', 'decimalLongitude', 'decimalLatitude', 'year', 'basisOfRecord']]
-    gbif_data['year'] = gbif_data['year'].astype('Int64')
-    gbif_data.to_csv(TEMP_FOLDER+'distrib_'+datafile+'.useless', mode="w", header=True, index=False)
+    gbif_data = pd.read_csv(DATA_FOLDER + datafile + ".zip", sep="\t")
+    gbif_data = gbif_data[
+        ["speciesKey", "decimalLongitude", "decimalLatitude", "year", "basisOfRecord"]
+    ].drop_duplicates()
+    gbif_data = gbif_data.merge(
+        taxalist, left_on="speciesKey", right_on="gbifID", how="left"
+    )
+    gbif_data = gbif_data[
+        ["taxonID", "decimalLongitude", "decimalLatitude", "year", "basisOfRecord"]
+    ]
+    gbif_data["year"] = gbif_data["year"].astype("Int64")
+    gbif_data.to_csv(
+        TEMP_FOLDER + "distrib_" + datafile + ".useless",
+        mode="w",
+        header=True,
+        index=False,
+    )
 
 
 # Merging everything and exporting as one clean database file
 distrib = (
-            pd.concat(
-                    (pd.read_csv(TEMP_FOLDER+'distrib_'+f+'.useless') for f in LIST_OF_DATA_FILES),
-                    ignore_index=True
-                )
-               .drop_duplicates()
-               .sort_values(['newID'])
+    pd.concat(
+        (
+            pd.read_csv(TEMP_FOLDER + "distrib_" + f + ".useless")
+            for f in LIST_OF_DATA_FILES
+        ),
+        ignore_index=True,
+    )
+    .drop_duplicates()
+    .sort_values(["taxonID"])
 )
-distrib.to_csv(DATABASE_FOLDER+'distrib.csv', mode="w", header=True, index=False)
+distrib["year"] = distrib["year"].astype("Int64")
+print("\n## distrib.head()\n", distrib.head())
+print("\n## distrib.describe()\n", distrib.describe())
+print(
+    "\n## distrib['basisOfRecord'].value_counts()\n",
+    distrib["basisOfRecord"].value_counts(),
+)
+
+assert distrib["taxonID"].isna().sum() == 0, "NAs in taxonID"
+assert distrib["decimalLongitude"].isna().sum() == 0, "NAs in decimalLongitude"
+assert distrib["decimalLatitude"].isna().sum() == 0, "NAs in decimalLatitude"
+
+distrib.to_csv(DATABASE_FOLDER + "distrib.csv", mode="w", header=True, index=False)
 
 
 # <============================================================================
